7634327745e42e393f2688f319e0e312d6e80aa8
Isaac Mora <165749027+isaacmora999@users.noreply.github.com>
2025-10-20 18:13:48 -0600

    Add files via upload

---
+++ JPA_relaciones/src/main/java/ni/edu/uam/jpa_relaciones/HelloApplication.java
@@ -0,0 +1,9 @@
+package ni.edu.uam.jpa_relaciones;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
